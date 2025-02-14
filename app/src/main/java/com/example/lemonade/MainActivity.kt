66a8c638613db37c559cc4c3e904178f242de71a
Jonathan Langdon <langdon.jonathan@gmail.com>
2025-02-14 10:51:51 -0500

    first commit, readme and initial project files

---
+++ app/src/main/java/com/example/lemonade/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.example.lemonade
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.lemonade.ui.theme.LemonadeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        enableEdgeToEdge()
+        super.onCreate(savedInstanceState)
+        setContent {
+            LemonadeTheme {
+                LemonadeApp()
+            }
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun LemonadeApp() {
+
+}
+
+@Composable
+fun LemonTextAndImage(
+
+) {
+
+}
+
+@Preview
+@Composable
+fun LemonPreview() {
+    LemonadeTheme() {
+        LemonadeApp()
+    }
+}
